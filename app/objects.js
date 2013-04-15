4cc90d996f9ecbdd1a5d3adbea8271ee90dc3061
Olga Torkhanova <helgablazhkun@gmail.com>
2013-04-15 11:06:25 +0400

    "object and context" section was finished

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,24 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      return fn.call(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-
+        constructor.prototype.greeting=greeting;
     },
 
     iterate : function(obj) {
-
+      var result=[];
+      var i=0;
+      for(var key in obj)
+      {
+      	if (obj.hasOwnProperty(key)) {
+      	  result[i]=key+": "+obj[key];
+      	  i++;
+        }
+      }
+      return result;
     }
   };
 });
